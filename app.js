1940ed41bb19543025306716d19333e456abef1d
Shreys2021 <shreyasaid48@gmail.com>
2021-08-28 20:36:36 +0530

    add start script

--- app.js
+++ app.js
@@ -170,6 +170,8 @@ app.use((err, req, res, next) => {
 
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log('listening on port 3000');
 })
